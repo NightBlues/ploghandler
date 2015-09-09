26b13ea852294a27cd31e989caefb981a1c3d7bd
Vadim Radovel <vadim@radovel.ru>
2015-09-09 14:47:49 +0300

    Prepare for pypi.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 PACKAGE_PATH = os.path.abspath(os.path.dirname(__file__))
 
 def get_description():
-    with open(os.path.join(PACKAGE_PATH, "README.md")) as readme:
+    with open(os.path.join(PACKAGE_PATH, "README.rst")) as readme:
         return readme.read()
 
 setup(
@@ -16,9 +16,11 @@ setup(
     author_email="vadim@radovel.ru",
     description="Provides concurrent rotating file handler for posix-compatible OSes.",
     license="GPLv3",
+    url="https://github.com/NightBlues/ploghandler",
     py_modules=["ploghandler"],
     test_suite="test",
     long_description=get_description(),
+    platforms = ["posix"],
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
