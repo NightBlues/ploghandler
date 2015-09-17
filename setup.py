f7f82cbdb78e16a611b1124b64fff0888c55c40e
Vadim Radovel <vadim@radovel.ru>
2015-09-17 17:04:06 +0300

    ver 0.4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def get_description():
 
 setup(
     name="ploghandler",
-    version="0.3",
+    version="0.4",
     author="Vadim Radovel",
     author_email="vadim@radovel.ru",
     description="Provides concurrent rotating file handler for posix-compatible OSes.",
